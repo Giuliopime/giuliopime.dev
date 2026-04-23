c6737ac8f0062eb518f29ed405946c45171cc73d
Giuliopime <giuliopime@gmail.com>
2026-04-23 22:06:43 +0200

    blog and guides related project

--- app/pages/guides/[slug].vue
+++ app/pages/guides/[slug].vue
@@ -3,6 +3,17 @@ const slug = useRoute().params.slug
 const { data: post } = await useAsyncData(`guide-${slug}`, () => {
 	return queryCollection('guides').path(`/guides/${slug}`).first()
 })
+
+const { data: projects } = await useAsyncData('projects-list', () => {
+  return queryCollection('projects')
+      .order('date', 'DESC')
+      .select('title', 'path', 'description', 'date', 'tags', 'major')
+      .all()
+})
+
+const relatedProject = computed(() => {
+  return projects.value?.find((a) => a.title?.toLowerCase() === post.value?.project?.toLowerCase())
+})
 </script>
 
 <template>
@@ -13,6 +24,27 @@ const { data: post } = await useAsyncData(`guide-${slug}`, () => {
     </div>
 
     <ContentRenderer tag="article" :value="post" class="prose md:prose-xl dark:prose-invert max-w-none md:max-w-prose w-full min-w-0" />
+
+    <div class="flex flex-col justify-start mt-20" v-if="relatedProject">
+      <span class="text-coral font-sohne text-xs py-2">/ RELATED-PROJECT</span>
+
+      <div class="grid grid-cols-[1fr_auto] gap-x-8">
+        <div class="col-span-2 border-b border-white/50"></div>
+        <nuxt-link
+            :to="relatedProject.path"
+            class="group col-span-2 grid grid-cols-subgrid gap-x-4 sm:gap-x-8 hover:bg-sky hover:text-black cursor-pointer py-3 items-start"
+        >
+            <span class="hidden sm:block opacity-80 whitespace-nowrap">
+              {{ new Date(relatedProject.date).toLocaleDateString() }}
+            </span>
+          <div class="flex flex-col gap-1.5 pr-4 sm:pr-8">
+            <span class="line-clamp-2 leading-snug">{{ relatedProject.title }}</span>
+            <span class="line-clamp-3 sm:line-clamp-2 leading-snug opacity-75">{{ relatedProject.description }}</span>
+          </div>
+        </nuxt-link>
+        <div class="col-span-2 border-b border-white/50"></div>
+      </div>
+    </div>
   </div>
 </template>
 
